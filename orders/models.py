95673b5a7ba6ec017e17b0753ebcc6948c20a28e
ant <enter4p@gmail.com>
2025-06-13 16:47:32 +0300

    order model + settings update

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заказ")
+    slug = models.SlugField(max_length=220, verbose_name="Slug")
+    quantity = models.PositiveSmallIntegerField(default=1, verbose_name="Количество")
+    material = models.CharField(max_length=200, verbose_name="Материал")
+    client = models.CharField(max_length=200, verbose_name="Клиент")
+
+    class Meta:
+        db_table = "orders"
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return str(self.title)
