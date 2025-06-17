a682719f6cd9fcc9e0b2928d473581d0441f7b45
ant <enter4p@gmail.com>
2025-06-17 20:02:23 +0300

    time calculate add

--- orders/models.py
+++ orders/models.py
@@ -1,13 +1,24 @@
+from datetime import datetime
 from django.db import models
 from django.urls import reverse
 
 
 class Order(models.Model):
+
     title = models.CharField(max_length=200, verbose_name="Заказ")
     slug = models.SlugField(max_length=220, verbose_name="Slug")
     quantity = models.PositiveSmallIntegerField(default=1, verbose_name="Количество")
     material = models.CharField(max_length=200, verbose_name="Материал")
     client = models.CharField(max_length=200, verbose_name="Клиент")
+    start_time = models.DateTimeField(
+        default=datetime.now, blank=True,
+        verbose_name="Начало выполнения"
+    )
+    end_time = models.DateTimeField(
+        default=datetime.now, blank=True,
+        verbose_name="Конец выполнения"
+    )
+    # full_time = models.TimeField(editable=False, blank=True, verbose_name='Общее время')
 
     class Meta:
         db_table = "orders"
@@ -20,3 +31,9 @@ class Order(models.Model):
 
     def get_success_url(self):
         return reverse("order", kwargs={"slug": self.object.slug})
+
+    @property
+    def time_difference(self):
+        if self.start_time and self.end_time:
+             return self.end_time - self.start_time
+        return None
