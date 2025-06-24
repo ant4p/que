fe66035cb1fb80c77d0d1cd0f8acea05a63d5d61
ant <enter4p@gmail.com>
2025-06-24 14:05:18 +0300

    logig report fixes

--- reports/views.py
+++ reports/views.py
@@ -20,23 +20,16 @@ class ReportView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context)
         instanse = Report.objects.get(slug=self.kwargs["slug"])
-        start_day = str(instanse.start_day)
-        print("taatatat")
-        print("taatatat")
-        print("taatatat")
-        print("taatatat")
-        print(start_day)
-        end_day = str(instanse.end_day)
-        print(end_day)
-        orders = (
-            Order.objects.filter(start_time__date__gte=start_day)
-            .filter(end_time__date__lte=end_day)
-            # .filter(printers__slug=instanse.printers)
-        )
 
-        context["orders_data"] = orders
         printers = Printer.objects.filter(report__slug=self.kwargs["slug"])
         context["printers_data"] = printers
+
+        orders = (
+            Order.objects.filter(start_time__date__gte=str(instanse.start_day))
+            .filter(end_time__date__lte=str(instanse.end_day))
+            .filter(printers__in=instanse.printers.all())
+        )
+        context["orders_data"] = orders
+
         return context
