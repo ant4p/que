f9989cceee3a78e5008f482f77731586c355415a
ant <enter4p@gmail.com>
2025-07-04 20:54:49 +0300

    save method update test

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.db.models import Q
 from django.urls import reverse
 
 from core.models import BaseModel
@@ -43,6 +44,21 @@ class Order(BaseModel):
         verbose_name = "Заказ"
         verbose_name_plural = "Заказы"
         ordering = ["-id"]
+
+    def order_unique(self, model, start_time, end_time, **kwargs):
+        query = Q(start_time__lte=end_time, end_time_gte=start_time)
+        if kwargs:
+            query &= Q(**kwargs)
+        return model.objects.filter(query).exists()
+
+    
+    def save(self, *args, **kwargs):
+        # date_time = Order.objects.filter(Q(start_time__lte=self.start_time) & Q(end_time__gte=self.end_time))
+        # print(date_time)
+        if Order.objects.filter(Q(start_time__lte=self.start_time) & Q(end_time__gte=self.end_time)):
+             super().save(*args, **kwargs)
+        else:
+            print('NONONO')
 
     def __str__(self):
         return str(self.title)
